57476c08f5ae69b0448d075b0291711d017865f5
kevindimayuga <k.dimayuga22@gmail.com>
2023-08-07 17:04:46 -0500

    created variables for each package needed for this application

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // TODO: Include packages needed for this application
 
+// fs package into const variable
+const fs = require('fs');
+
+// inquirer package into const variable
+const inquirer = require('inquirer');
+
+// markdown js file into const variable
+const generateMarkdown = require('./Develop/utils/generateMarkdown')
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
